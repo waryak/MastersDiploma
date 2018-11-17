cc4236e75ebcae6e9edcf6f65958c74a2a457dbb
waryak <waryak2012@mail.ru>
2018-11-17 17:12:30 +0300

    Wishart fix

--- source/wishart.py
+++ source/wishart.py
@@ -16,16 +16,33 @@ class Wishart:
         self.significant_clusters = set()
 
     def _fit_kd_tree(self, data):
+        """
+        Fit KDTree for z-vectors
+        :param data:
+        :return:
+        """
         kdt = cKDTree(data=data)
         return kdt
 
     def _n_dim_ball_volume(self, radius):
+        """
+        Calculates the volume of the ball
+        :param radius: the distance to the furthest neighbor
+        :return:
+        """
+
         volume = (radius ** self.k) * (np.pi ** (self.k / 2)) / gamma(self.k / 2 + 1)
-        return volume * self.k / self.clusters.shape[0]
+        return self.k / self.clusters.shape[0] / volume
 
     def _check_cluster_significance(self, cluster, matrix_distances):
         """
+        Checks the cluster significance
+        :param cluster: number of cluster to check
+        :param matrix_distances: matrix of distances
+        :return: Returns bool. True - if cluster is significant and was added to the list.
+                               False - if cluster is not significant and was not added to the list.
         """
+        assert cluster in self.clusters, "There is no presence of cluster %i in the graph yet" %cluster
         verticies_radiuses = matrix_distances[self.clusters == cluster][:, self.k - 1]
         vertex_with_biggest_radius = max(verticies_radiuses)
         vertex_with_smalles_radius = min(verticies_radiuses)
@@ -33,11 +50,17 @@ class Wishart:
         ball_volume_min = self._n_dim_ball_volume(vertex_with_smalles_radius)
         maximum_difference = abs(ball_volume_max - ball_volume_min)
         if maximum_difference > self.h:
-            self.significant_clusters.add(cluster)
+            return True
         else:
-            pass
+            return False
 
     def _construct_neighbors_matrix(self, data, kdtree):
+        """
+
+        :param data:
+        :param kdtree:
+        :return:
+        """
         zvector_neighbors_distances = np.zeros(shape=(data.shape[0], self.k))
         zvector_neighbors_indexes = np.empty(shape=(data.shape[0], self.k))
         self.clusters = np.zeros(data.shape[0], dtype=int)
@@ -45,23 +68,37 @@ class Wishart:
             zvector_neighbors_dist, zvector_neighbors_ind = kdtree.query(x=zvector, k=self.k + 1)
             zvector_neighbors_distances[index] = zvector_neighbors_dist[1:]
             zvector_neighbors_indexes[index] = zvector_neighbors_ind[1:]
-        # sort matricies
+        # sort matrix by ascending of the distance to the furthest NN
         vertecies_sorted = zvector_neighbors_distances[:, -1].argsort()
         matrix_distances_sorted = zvector_neighbors_distances[vertecies_sorted]
         matrix_indexes_sorted = zvector_neighbors_indexes[vertecies_sorted]
         return matrix_distances_sorted, matrix_indexes_sorted, vertecies_sorted
 
     def _form_new_cluster(self, vertex):
+        """
+        Forms a new cluster out of a new vertex
+        :param vertex:
+        :return: number of cluster
+        """
         max_cluster = self.clusters.max() + 1
         self.clusters[vertex] = max_cluster
+        return max_cluster
 
     def _find_connections(self, vertex, vertex_neighbors, matrix_indecies):
+        """
+        Finds all connection of this vertex in a graph.
+        :param vertex: vertex we want to find connections for.
+        :param vertex_neighbors: a list of vertex neighbors.
+        :param matrix_indexes: a matrix of all neighbors indexes for each vertex
+        :return: vertex_to_g_connections - array of vertex indexes which are connected to the current vertex
+                 vertex_to_g_connections_clusters - array of cluster labels of vertexes in vertex_to_g_connections
+        """
         # Check if existing nodes are connected to a new-comming node
         node_in_g_connections = matrix_indecies[self.G]
         vertex_to_g_connections = np.any(a=(node_in_g_connections == vertex), axis=1)
         if vertex_to_g_connections.shape[0] > 0:
             vertex_to_g_connections = self.G[vertex_to_g_connections]
-        # Check if new-comming node has neighbors among existing nodes
+        # Check if new-coming node has neighbors among existing nodes
         for neighbor in vertex_neighbors:
             if neighbor in self.G:
                 vertex_to_g_connections = np.append(arr=vertex_to_g_connections, values=neighbor)
@@ -79,45 +116,49 @@ class Wishart:
 
             # Check if vertex is isolated
             if len(unique_clusters) == 0:
-                self._form_new_cluster(vertex=vertex)
+                _ = self._form_new_cluster(vertex=vertex)
             # If vertex has only connection to one cluster, then:
-            elif len(vertex_connections) == 1:
-                vertex_connection = vertex_connections[0]
+            elif len(unique_clusters) == 1:
+                connection_cluster = list(unique_clusters)[0]
                 # If cluster is already completed
-                if vertex_connection in self.clusters_completenes:
+                if connection_cluster in self.clusters_completenes:
                     self.clusters[vertex] = 0
                 # If cluster is not completed
                 else:
-                    connection_cluster = self.clusters[vertex_connection]
                     self.clusters[vertex] = connection_cluster
-                    self._check_cluster_significance(connection_cluster, m_d)
+                    if self._check_cluster_significance(connection_cluster, m_d):
+                        # Add cluster to list of significant clusters
+                        self.significant_clusters.add(connection_cluster)
             # If vertex is connected to more than one clusters/vertcies
             else:
-                connected_clusters = set(self.clusters[vertex_connections])
                 # If all connections are completed cluster, than assign vertex to zero
-                if all(map(lambda x: x in self.clusters_completenes, connected_clusters)):
+                if all(map(lambda x: x in self.clusters_completenes, unique_clusters)):
                     self.clusters[vertex] = 0
                 # If one of the clusters is zero, or there are more than one significant clusters,
                 # then assign new vertex to zero
-                elif (min(connected_clusters) == 0) | \
-                        (len(connected_clusters.intersection(self.significant_clusters)) > 1):
+                elif (min(unique_clusters) == 0) | \
+                     (len(unique_clusters.intersection(self.significant_clusters)) > 1):
                     self.clusters[vertex] = 0
-                    insignificant_to_zero = connected_clusters.difference(self.significant_clusters)
-                    significant_to_completed = connected_clusters.intersection(self.significant_clusters)
+                    insignificant_to_zero = unique_clusters.difference(self.significant_clusters)
+                    significant_to_completed = unique_clusters.intersection(self.significant_clusters)
                     self.clusters_completenes = self.clusters_completenes.union(significant_to_completed)
                     for cluster in insignificant_to_zero:
                         self.clusters[self.clusters == cluster] = 0
                 # If there is one or less significant class and no zero classes,
-                # then we should collapse all clusters including new-comming node
+                # then we should collapse all clusters including new-coming node
                 # to the oldest cluster(oldest means that it has the biggest density)
+                # TODO: Think of the oprimisation in "CONNECTION SEARCH"
+                # TODO: DO NOT COLLAPSE ALL CLUSTERS. DO NOT TOUCH COMPLETED CLUSTERS!
                 else:
-                    oldest_cluster = min(connected_clusters)
-                    other_clusters = sorted(list(connected_clusters))[1:]
+                    oldest_cluster = min(unique_clusters)
+                    # Exclude completed clusters
+                    unique_clusters = unique_clusters.difference(self.clusters_completenes)
+                    other_clusters = sorted(list(unique_clusters))[1:]
                     for cluster in other_clusters:
                         self.clusters[self.clusters == cluster] = oldest_cluster
                     self.clusters[vertex] = oldest_cluster
-                    self._check_cluster_significance(cluster=oldest_cluster, matrix_distances=m_d)
-
+                    if self._check_cluster_significance(cluster=oldest_cluster, matrix_distances=m_d):
+                        self.significant_clusters.add(oldest_cluster)
             self.G = np.append(arr=self.G, values=vertex)
 
     def _form_cluster_centers(self, data):
