9d7301d9286106363e7e6247fceef7bf8b4bc076
waryak <waryak2012@mail.ru>
2019-01-08 16:41:39 +0300

    Dockerized wishart producer. THis version doesn't work - hangs up before producing tasks

--- src/network/messages/celery.py
+++ src/network/messages/celery.py
@@ -1,5 +1,25 @@
+from os import environ
 from celery import Celery
+
+print("--------------- LOADING ENVIRONMENT VARIABLES ---------------")
+PRODUCER_BROKER_URL = environ.get('CONFIG')
+if PRODUCER_BROKER_URL is None:
+    raise Exception("Broker URL for producer could not be found")
+else:
+    print("-> Producer's url is:", PRODUCER_BROKER_URL)
+WORKER_BROKER_URL = environ.get('CONFIG')
+if WORKER_BROKER_URL is None:
+    raise Exception("Broker URL for worker could not be found")
+else:
+    print("-> Worker's url is:", WORKER_BROKER_URL)
 app = Celery('test_celery',
-             broker='amqp://admin:mypass@rabbit:5672',
+             broker=PRODUCER_BROKER_URL,
              backend='rpc://',
-             include=['test_celery.tasks'])
+             include=['messages.tasks'])
+
+
+
+
+
+
+# app = Celery('test_celery',broker='amqp://admin:mypass@rabbit:5672',backend='rpc://',include=['test_celery.tasks'])
--- src/network/messages/run_tasks.py
+++ src/network/messages/run_tasks.py
@@ -1,12 +1,14 @@
-from .tasks import longtime_add
 import time
+from .tasks import longtime_add
+
+
 if __name__ == '__main__':
+
+    print("----------------- STARTING SUBMITTING TASKS -----------------")
     for count in range(30):
         result = longtime_add.delay(count)
-        print('Task finished?',result.ready())
-        print('Task result:',result.result)
+        print('Task finished?', result.ready())
+        print('Task result:', result.result)
         time.sleep(5)
-        print('Task finished"',result.ready())
-        print('Task result:',result.result)
-
-
+        print('Task finished"', result.ready())
+        print('Task result:', result.result)
