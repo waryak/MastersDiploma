3bfab496c02e6663bbcd50c5f02feae01966cdd3
waryak <waryak2012@mail.ru>
2019-01-09 03:59:20 +0300

    dockerized worker (Working edition)

--- src/network/messages/run_tasks.py
+++ src/network/messages/run_tasks.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
 
     print("----------------- STARTING SUBMITTING TASKS -----------------")
     for count in range(30):
+        print("Count is ", count)
         result = longtime_add.delay(count)
         print('Task finished?', result.ready())
         print('Task result:', result.result)
