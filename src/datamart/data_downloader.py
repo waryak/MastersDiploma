84b1022e2ff91b2cab1ac5c79a7e7ec717289b5d
waryak <waryak2012@mail.ru>
2019-01-14 00:04:15 +0300

    Adding DataManager

--- src/datamart/data_downloader.py
+++ src/datamart/data_downloader.py
@@ -1,7 +1,18 @@
 import pandas as pd
+from src.algo
 
 
 class DataManager:
     """
     Class to prepare data structure, directories and download data itself
     """
+    def __init__(self, PATH_TO_TS, PATH_TO_MODELS):
+        self.PATH_TO_TS = PATH_TO_TS
+        self.PATH_TO_MODELS = PATH_TO_MODELS
+
+    def _download_generate_lorenz(self):
+        """
+        Generate modeled lorenz time series
+        :return:
+        """
+
