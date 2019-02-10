1df4bfe0ea7f53515c4008c85ee0cedf38cda955
Alexander Vorontsov <voronchov@telemed.help>
2019-02-10 15:11:47 +0300

    added mongo manager

---
+++ src/datamart/mongo_manager.py
@@ -0,0 +1,43 @@
+from urllib.parse import quote_plus as quote
+import ssl
+import pymongo
+
+def singleton(class_):
+    instances = {}
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
+
+@singleton
+class MongoManager:
+    def __init__(self):
+        '''Initiate mongoDB connection
+        TODO: get rid of hard coded params
+        Make a config with params'''
+        url = 'mongodb://{user}:{pw}@{hosts}/?replicaSet={rs}&authSource={auth_src}'.format(
+            user=quote('user1'),
+            pw=quote('diploma_test'),
+            hosts=','.join([
+                'rc1c-fa410ox5e6dab6yw.mdb.yandexcloud.net:27018'
+            ]),
+            rs='rs01',
+            auth_src='db1')
+        self.dbs = pymongo.MongoClient(
+            url,
+            ssl_ca_certs='/usr/local/share/ca-certificates/Yandex/YandexInternalRootCA.crt',
+            ssl_cert_reqs=ssl.CERT_REQUIRED)['db1']
+    
+    def get_test_lorenz(self):
+        '''Get lorenz28 series from test column'''
+        col = self.dbs['lorenz28']
+        return col.find_one()
+    
+    def insert_document(self,column_name,document):
+        '''insert any JSON-serializable document
+        params:
+        column_name - string
+        document - JSON serializable dict
+        '''
+        self.dbs[column_name].insert_one(document)
